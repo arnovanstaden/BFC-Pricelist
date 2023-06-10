637b778557b144fb97325801e3a6f393d9b36d8c
arnovanstaden <arnovanstaden@gmail.com>
2023-06-10 08:54:42 +0200

    remove directory

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 import Header from '@components/Header';
 import AuthWrapper from '@components/AuthWrapper';
 import Notification from '@components/atoms/Notification';
+import { Grid, Container } from '@my-mui/material';
+import SideBar from './components/SideBar';
 
 export const metadata: Metadata = {
   title: 'Dashboard | The Beautox and Filler Clinic',
@@ -12,7 +14,23 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
     <AuthWrapper>
       <Header admin />
       <main>
-        {children}
+        <Grid container>
+          <Grid item xs={12} sm={3} lg={2}>
+            <SideBar />
+          </Grid>
+
+          <Grid item justifyContent="center" xs={12} md={9} lg={10}>
+            <Container
+              maxWidth="lg"
+              sx={{
+                paddingTop: '2rem',
+                paddingBottom: '2rem',
+              }}
+            >
+              {children}
+            </Container>
+          </Grid>
+        </Grid>
       </main>
 
       <Notification />
